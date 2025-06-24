e0f8e7ce1c90415176734702ad51df2c3edc94d0
Hayotjon Sultonov <hayotjonsultonov2005@mail.ru>
2025-06-24 20:11:39 +0500

    Add meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import Header from "@components/shared/Header";
 import { Footer } from "./components/sections";
 import { ToastContainer } from "react-toastify";
